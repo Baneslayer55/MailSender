b99c5291193041cff26534e578c8fc4f16031957
Baneslayer55 <65244708+Baneslayer55@users.noreply.github.com>
2020-09-15 18:01:04 +0300

    added singleton

---
+++ MailSender/Models/MailConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailSender.Models
+{
+    public class MailConfig
+    {
+        private static MailConfig instance;
+
+        public static MailConfig GetMailConfig()
+        {
+            if (instance == null)
+            {
+                instance = Js;
+            }
+            return instance;
+        }
+
+    }
+}
